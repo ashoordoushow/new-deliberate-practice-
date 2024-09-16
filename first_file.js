e0faed2b138f4404464d713f437667360cb8d58f
ashoordoushow <ashoor12323@gmail.com>
2024-09-16 13:46:40 -0500

    loops practice js

--- first_file.js
+++ first_file.js
@@ -60,16 +60,23 @@ var fruits = ["apple", "pear", "honeydew", "durian"]; // this is an array writte
 
 var i = 0;
 
-while (i < fruits.length) {
+while (i < fruits.length) {  // this is the while loop in javascript
 //   console.log('in the loop');
   console.log(fruits[i]); // this prints out all the fruits in the array
   // i = i + 1; this is the same as the line below
 //   i += 1;
   i++; // this is the same as the above two lines. AND IS how you are supposed to type it when using javacript
 }
+//  THIS IS A GOOD LOOP TO USE BECAUSE YOU CAN USE OTHER TYPES OF FUNCTIONS
 
+fruits.forEach(function(fruit, i) {
+//   console.log(i); // you can also print out 'i' in the forEach loop  // this is the the 'forEach' loop and instead of using underscores like ruby (for_each), we use snakeprint for javascript (forEach)
+  console.log(fruit); // THIS IS THE LOOP WE'RE GOING TO BE USING THE MOST!
+});  // there is no 'end' here like in ruby. the end it this '});'
 
-
+// for (var i = 0; i < fruits.length; i++) {  // this is the 'for' loop in js script
+//   console.log(fruits[i]);} // console.log(i); this prints '0-3', console.log(fruits[i]); this prints each value in the fruits array
+// We are not going to be using this loop (old school loop)
 // console.log(fruits[0]); // this is how you print in javascript format. typing fruite in the parenthesis prints out the full arrray, you can also put a string in their to print, and to print out a specific item in the array you type the variable + [] and the the location of that value Ex. console.log(fruits[0])
 
 
