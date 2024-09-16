ed6870ed55bd0aec9d7ae361f6630e0cbdbb28c6
ashoordoushow <ashoor12323@gmail.com>
2024-09-16 12:17:15 -0500

    javascript review lecture practice

---
+++ first_file.js
@@ -0,0 +1,76 @@
+// console.log('hello');
+
+// Write a program that uses a variable to store a number, then prints that number plus 10.
+
+// var number = 2;
+// console.log(number + 10);
+
+
+
+// var x = 5
+// var y = 10
+// console.log(x + y)
+
+// var x = 'hello world'
+// console.log(x)
+
+// var x = (6 + 30) * 20
+// console.log(x)
+
+// var x = x * 10
+// console.log(x)
+
+// const firstName = 'bob'
+// const lastName = 'mitchell'
+// const fullName = `${firstName} ${lastName}`
+// console.log(fullName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// This package has installed:
+// 	•	Node.js v20.17.0 to /usr/local/bin/node
+// 	•	npm v10.8.2 to /usr/local/bin/npm
+// Make sure that /usr/local/bin is in your $PATH.
+
+
+
+// make a variable 
+
+var fruits = ["apple", "pear", "honeydew", "durian"]; // this is an array written in javascript format
+
+
+
+//loop through and print out each item in the array
+// declare a variable
+// write while with condition
+// write code in loop
+// print inside of loop
+// increment variable //write the parenthesis and curly brackets at the beginning when writing a loop
+
+var i = 0;
+
+while (i < fruits.length) {
+//   console.log('in the loop');
+  console.log(fruits[i]); // this prints out all the fruits in the array
+  // i = i + 1; this is the same as the line below
+//   i += 1;
+  i++; // this is the same as the above two lines. AND IS how you are supposed to type it when using javacript
+}
+
+
+
+// console.log(fruits[0]); // this is how you print in javascript format. typing fruite in the parenthesis prints out the full arrray, you can also put a string in their to print, and to print out a specific item in the array you type the variable + [] and the the location of that value Ex. console.log(fruits[0])
+
+
+
--- line1_basic_practice.rb
+++ line1_basic_practice.rb
@@ -1,1 +1,63 @@
-p "hello"
+# Write a program that uses a variable to store a number, then prints that number plus 10.
+
+# x = 0
+# if x <= 10
+#     x = x + 10
+#     p x
+# end
+
+# p "hello"
+
+# Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+# x = "bob"
+# p x.upcase
+
+# Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+# x = 10
+# y = 20
+# z = x + y
+# p z
+
+# Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+# x = "popular"
+# p x.reverse
+
+# Write a program that uses a variable to store a number, then prints the number times 10.
+
+# number = 100
+# puts number * 10
+
+# Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+# x = "bob"
+# y = "snow"
+# z = x + y 
+# p z.upcase
+
+# word1 = "hello"
+# word2 = "world"
+# puts word1.upcase + word2.upcase
+
+# Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+# word = "hello"
+# puts word.length
+
+#OR
+
+# x = "hello"
+# p x.chars.count
+
+# Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+x = 4.3
+p x.to_i
+
+number = 19.99
+puts number.to_i
+
+x = 10.to_f  
+p x
